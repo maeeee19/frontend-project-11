bb772eb75ca2ed7d56cfa2eeea9b39d25fc39fbe
Arina Maslanova <maslanovaa@mail.ru>
2025-07-07 09:35:48 +0300

    fix cache

--- src/api.js
+++ src/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const fetchFeed = (url) => {
   const proxyUrl = new URL('https://allorigins.hexlet.app/get');
   proxyUrl.searchParams.set('url', url);
+  proxyUrl.searchParams.set('disableCache', 'true');
 
   return axios.get(proxyUrl.toString());
 };
